b5e96895adf454a157b5973ceb7fc7f0bdc39ed2
yashsb-07 <yashsbansode07@gmail.com>
2026-03-18 22:52:00 +0530

    Add hero image slider to showcase embroidery designs

--- js/script.js
+++ js/script.js
@@ -6,3 +6,23 @@ function openLightbox(src){
 function closeLightbox(){
     document.getElementById("lightbox").style.display="none";
 }
+
+// HERO SLIDER
+
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function showSlide(){
+
+    slides.forEach(slide => slide.classList.remove("active"));
+
+    index++;
+
+    if(index >= slides.length){
+        index = 0;
+    }
+
+    slides[index].classList.add("active");
+}
+
+setInterval(showSlide, 4000);
